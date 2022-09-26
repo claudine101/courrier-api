a493b82fb15e21eb1672a829b2c7e5ece33e95a5
95innocent <ndayikengurukiyeinnocent95@gmail.com>
2022-09-26 19:47:47 +0200

    Listing category by id

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -4,6 +4,8 @@ const productsController = require("../controllers/products.controller.js")
 
 const productsRouter = express.Router()
 productsRouter.get('/', productsController.getAllProducts)
+productsRouter.get('/:ID_PRODUIT_PARTENAIRE', productsController.getOne)
+productsRouter.get('/categories/:ID_PARTENAIRE', productsController.getproducts)
 productsRouter.get('/categories', productsController.getAllCategorie)
 productsRouter.get('/sub_categories/:ID_CATEGORIE_PRODUIT', productsController.getSousCategoriesBy)
 productsRouter.get('/sizes/:ID_CATEGORIE_PRODUIT', productsController.getSizes)
