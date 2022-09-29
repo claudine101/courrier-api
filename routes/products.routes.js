9d11c1b2ea1d9e45ed4a8f312edb9fa08422d35c
claudine101 <ndayisabaclaudine01@gmail.com>
2022-09-29 11:57:32 +0200

    affichagege des tetails pour un boutique

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const productsController = require("../controllers/products.controller.js")
-
-
 const productsRouter = express.Router()
 productsRouter.get('/', productsController.getAllProducts)
 productsRouter.get('/product/:ID_PRODUIT_PARTENAIRE', productsController.getOne)
+productsRouter.get('/products/:ID_PARTENAIRE', productsController.getbyID)
 productsRouter.get('/categories/:ID_PRODUIT_PARTENAIRE', productsController.getCategorieByPartenaire)
 productsRouter.get('/categories', productsController.getAllCategorie)
 productsRouter.get('/sub_categories/:ID_CATEGORIE_PRODUIT', productsController.getSousCategoriesBy)
