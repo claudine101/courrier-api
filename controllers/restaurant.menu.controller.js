e5bbcc25b8038f0a0d30d9adcae545744b45c14e
vanny <vdevelopper@gmail.com>
2022-10-04 18:45:54 +0200

    creation de route de recuperation des menus

--- controllers/restaurant.menu.controller.js
+++ controllers/restaurant.menu.controller.js
@@ -8,13 +8,18 @@ const path = require("path");
 const MenuUpload = require("../class/uploads/MenuUpload")
 const createMenu = async (req, res) => {
     try {
-        const {     
+        const {
             ID_CATEGORIE_MENU,
             ID_SOUS_CATEGORIE_MENU,
             ID_SOUS_SOUS_CATEGORIE,
-            ID_ROPAS,
+            ID_REPAS,
             ID_PARTENAIRE,
-            // ID_TAILLE,
+            DESCRIPTION_REPAS,
+            DESCRIPTION_FOURNISSEUR,
+            QUANTITE,
+            DESCRIPTION,
+            MONTANT,
+            ID_UNITE
         } = req.body
         const { IMAGE_1, IMAGE_2, IMAGE_3 } = req.files || {}
         const validation = new Validation(
@@ -24,28 +29,9 @@ const createMenu = async (req, res) => {
                     required: true,
                     image: 21000000
                 },
-                IMAGE_2: {
-                    required: true,
-                    image: 21000000
-                },
-                IMAGE_3: {
-                    required: true,
-                    image: 21000000
-                },
                 ID_CATEGORIE_MENU:
                 {
                     exists: "restaurant_categorie_menu,ID_CATEGORIE_MENU",
-                },
-                ID_SOUS_SOUS_CATEGORIE:
-                {
-                    exists: "restaurant_sous_categorie_menu,ID_SOUS_SOUS_CATEGORIE",
-                },
-               
-                ID_ROPAS:{
-                    exists: "restaurant_repas,ID_REPAS",
-                },
-                ID_PARTENAIRE:{
-                    exists: "partenaires,ID_PARTENAIRE",
                 },
             },
             {
@@ -53,30 +39,10 @@ const createMenu = async (req, res) => {
                     required: "Image d'un produit est obligatoire",
                     image: "taille invalide"
                 },
-                IMAGE_2: {
-                    required: "Image d'un produit est obligatoire",
-                    image: "taille invalide"
-                },
-                IMAGE_3: {
-                    required: "Image d'un produit est obligatoire",
-                    image: "taille invalide"
-                },
+
                 ID_CATEGORIE_MENU:
                 {
                     exists: "categorie invalide",
-                },
-                ID_SOUS_SOUS_CATEGORIE:
-                {
-                    exists: "sous categorie invalide",
-                },
-                ID_ROPAS:{
-                    exists: "repas  invalide",
-                },
-                ID_PARTENAIRE:{
-                    exists: "partenaire invalide",
-                },
-                ID_TAILLE:{
-                    exists: "taille invalide",
                 },
             }
         );
@@ -92,22 +58,45 @@ const createMenu = async (req, res) => {
             })
         }
         const menuUpload = new MenuUpload()
-                    const { fileInfo:fileInfo_1,thumbInfo:thumbInfo_1 } = await menuUpload.upload(IMAGE_1, false)
-                    const { fileInfo:fileInfo_2 ,thumbInfo:thumbInfo_2} = await menuUpload.upload(IMAGE_2, false)
-                    const { fileInfo:fileInfo_3,thumbInfo:thumbInfo_3} = await menuUpload.upload(IMAGE_3, false)
-        const { insertId} = await menuModel.createMenu(
+        const { fileInfo: fileInfo_1, thumbInfo: thumbInfo_1 } = await menuUpload.upload(IMAGE_1, false)
+        const { fileInfo: fileInfo_2, thumbInfo: thumbInfo_2 } = await menuUpload.upload(IMAGE_2, false)
+        const { fileInfo: fileInfo_3, thumbInfo: thumbInfo_3 } = await menuUpload.upload(IMAGE_3, false)
+        const { insertId } = await menuModel.createMenu(
             ID_CATEGORIE_MENU,
             ID_SOUS_CATEGORIE_MENU,
             ID_SOUS_SOUS_CATEGORIE,
-            ID_ROPAS,
+            ID_REPAS,
             ID_PARTENAIRE,
-            ID_TAILLE,
             fileInfo_1.fileName,
             fileInfo_2.fileName,
             fileInfo_3.fileName,
-            req.userId,    
+            req.userId,
         )
+
+        const { insertId:id_repas } = await menuModel.createRepas(
+            2,
+            2,
+            DESCRIPTION_REPAS,
+            DESCRIPTION_FOURNISSEUR
+        );
+
+        const { insertId:id_menu_taille } = await menuModel.createMenuTaille(
+            ID_CATEGORIE_MENU,
+            QUANTITE,
+            DESCRIPTION,
+            ID_UNITE
+        );
+
+        const { insertId: id_prix_categorie} = await menuModel.createMenuPrix(
+            MONTANT,
+            2,
+            insertId,
+            2
+        );
+
+
         const menu = (await menuModel.findById(insertId))[0]
+
         res.status(RESPONSE_CODES.CREATED).json({
             statusCode: RESPONSE_CODES.CREATED,
             httpStatus: RESPONSE_STATUS.CREATED,
@@ -147,8 +136,118 @@ const findByInsertId = async (req, res) => {
     }
 }
 
+const getRepas = async (req, res) => {
+    try {
+        const produits = await menuModel.findAllRepas()
+        res.status(RESPONSE_CODES.OK).json({
+            statusCode: RESPONSE_CODES.OK,
+            httpStatus: RESPONSE_STATUS.OK,
+            message: "succès",
+            result: produits
+        })
+    }
+    catch (error) {
+        console.log(error)
+        res.status(RESPONSE_CODES.INTERNAL_SERVER_ERROR).json({
+            statusCode: RESPONSE_CODES.INTERNAL_SERVER_ERROR,
+            httpStatus: RESPONSE_STATUS.INTERNAL_SERVER_ERROR,
+            message: "echoue",
+
+        })
+    }
+}
+
+const getCategories = async (req, res) => {
+    try {
+        const categories = await menuModel.findAllCategories()
+        res.status(RESPONSE_CODES.OK).json({
+            statusCode: RESPONSE_CODES.OK,
+            httpStatus: RESPONSE_STATUS.OK,
+            message: "succès",
+            result: categories
+        })
+    }
+    catch (error) {
+        console.log(error)
+        res.status(RESPONSE_CODES.INTERNAL_SERVER_ERROR).json({
+            statusCode: RESPONSE_CODES.INTERNAL_SERVER_ERROR,
+            httpStatus: RESPONSE_STATUS.INTERNAL_SERVER_ERROR,
+            message: "echoue",
+
+        })
+    }
+}
+
+const getSousCategories = async (req, res) => {
+    try {
+        const Souscategories = await menuModel.findAllSousCategories()
+        res.status(RESPONSE_CODES.OK).json({
+            statusCode: RESPONSE_CODES.OK,
+            httpStatus: RESPONSE_STATUS.OK,
+            message: "succès",
+            result: Souscategories
+        })
+    }
+    catch (error) {
+        console.log(error)
+        res.status(RESPONSE_CODES.INTERNAL_SERVER_ERROR).json({
+            statusCode: RESPONSE_CODES.INTERNAL_SERVER_ERROR,
+            httpStatus: RESPONSE_STATUS.INTERNAL_SERVER_ERROR,
+            message: "echoue",
+
+        })
+    }
+}
+
+const getSousSousCategories = async (req, res) => {
+    try {
+        const SousSouscategories = await menuModel.findAllSousSousCategories()
+        res.status(RESPONSE_CODES.OK).json({
+            statusCode: RESPONSE_CODES.OK,
+            httpStatus: RESPONSE_STATUS.OK,
+            message: "succès",
+            result: SousSouscategories
+        })
+    }
+    catch (error) {
+        console.log(error)
+        res.status(RESPONSE_CODES.INTERNAL_SERVER_ERROR).json({
+            statusCode: RESPONSE_CODES.INTERNAL_SERVER_ERROR,
+            httpStatus: RESPONSE_STATUS.INTERNAL_SERVER_ERROR,
+            message: "echoue",
+
+        })
+    }
+}
+
+const getUnites = async (req, res) => {
+    try {
+        const unites = await menuModel.findAllUnites()
+        res.status(RESPONSE_CODES.OK).json({
+            statusCode: RESPONSE_CODES.OK,
+            httpStatus: RESPONSE_STATUS.OK,
+            message: "succès",
+            result: unites
+        })
+    }
+    catch (error) {
+        console.log(error)
+        res.status(RESPONSE_CODES.INTERNAL_SERVER_ERROR).json({
+            statusCode: RESPONSE_CODES.INTERNAL_SERVER_ERROR,
+            httpStatus: RESPONSE_STATUS.INTERNAL_SERVER_ERROR,
+            message: "echoue",
+
+        })
+    }
+}
+
 module.exports = {
     createMenu,
     findByInsertId,
+    getRepas,
+    getCategories,
+    getSousCategories,
+    getSousSousCategories,
+    getUnites
 
 }
