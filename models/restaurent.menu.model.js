5614de8e28e9f1607e299bc39491483cc6b94ddd
vanny <vdevelopper@gmail.com>
2022-10-05 12:30:15 +0200

    recuperation des menus enregistrer

--- models/restaurent.menu.model.js
+++ models/restaurent.menu.model.js
@@ -1,11 +1,11 @@
 const { query } = require("../utils/db");
 
-const createMenu = (ID_CATEGORIE_MENU, ID_SOUS_CATEGORIE_MENU, ID_SOUS_SOUS_CATEGORIE, ID_REPAS, ID_PARTENAIRE, IMAGES_1, IMAGES_2, IMAGES_3, ID_USER
+const createMenu = (ID_CATEGORIE_MENU, ID_SOUS_CATEGORIE_MENU, ID_SOUS_SOUS_CATEGORIE, ID_REPAS, ID_PARTENAIRE,ID_MENU_QUANTITE, IMAGES_1, IMAGES_2, IMAGES_3, ID_USER
 ) => {
     try {
-        var sqlQuery = "INSERT INTO restaurant_menu (ID_CATEGORIE_MENU,ID_SOUS_CATEGORIE_MENU,ID_SOUS_SOUS_CATEGORIE,ID_REPAS,ID_PARTENAIRE,IMAGES_1,IMAGES_2,IMAGES_3,ID_USER)";
-        sqlQuery += "values (?,?,?,?,?,?,?,?,?)";
-        return query(sqlQuery, [ID_CATEGORIE_MENU, ID_SOUS_CATEGORIE_MENU, ID_SOUS_SOUS_CATEGORIE, ID_REPAS, ID_PARTENAIRE, IMAGES_1, IMAGES_2, IMAGES_3, ID_USER])
+        var sqlQuery = "INSERT INTO restaurant_menu (ID_CATEGORIE_MENU,ID_SOUS_CATEGORIE_MENU,ID_SOUS_SOUS_CATEGORIE,ID_REPAS,ID_PARTENAIRE,ID_MENU_QUANTITE,IMAGES_1,IMAGES_2,IMAGES_3,ID_USER)";
+        sqlQuery += "values (?,?,?,?,?,?,?,?,?,?)";
+        return query(sqlQuery, [ID_CATEGORIE_MENU, ID_SOUS_CATEGORIE_MENU, ID_SOUS_SOUS_CATEGORIE, ID_REPAS, ID_PARTENAIRE,ID_MENU_QUANTITE, IMAGES_1, IMAGES_2, IMAGES_3, ID_USER])
     }
     catch (error) {
 
@@ -53,9 +53,19 @@ const createMenuPrix = (MONTANT, ID_STATUT_PRIX, ID_RESTAURANT_MENU, ID_PRIX_COD
 }
 
 
+// const findById = async (id) => {
+//     try {
+//         return query("SELECT * FROM  restaurant_menu WHERE ID_RESTAURANT_MENU=?", [id]);
+//     } catch (error) {
+//         throw error;
+//     }
+// };
+
 const findById = async (id) => {
     try {
-        return query("SELECT * FROM  restaurant_menu WHERE ID_RESTAURANT_MENU=?", [id]);
+        // return query("SELECT * FROM  restaurant_menu_taille WHERE ID_MENU_QUANTITE=?", [id]);
+        // return query("SELECT * FROM restaurant_menu_taille LEFT JOIN restaurant_menu ON restaurant_menu.ID_MENU_QUANTITE=restaurant_menu_taille.ID_MENU_QUANTITE LEFT JOIN restaurant_categorie_menu ON restaurant_categorie_menu.ID_CATEGORIE_MENU=restaurant_menu.ID_CATEGORIE_MENU LEFT JOIN restaurant_sous_categorie_menu ON restaurant_sous_categorie_menu.ID_SOUS_CATEGORIE_MENU=restaurant_menu.ID_SOUS_CATEGORIE_MENU LEFT JOIN restaurant_sous_sous_categorie ON restaurant_sous_sous_categorie.ID_SOUS_SOUS_CATEGORIE=restaurant_menu.ID_SOUS_SOUS_CATEGORIE LEFT JOIN restaurant_repas ON restaurant_repas.ID_REPAS=restaurant_menu.ID_REPAS LEFT JOIN partenaires ON partenaires.ID_PARTENAIRE=restaurant_menu.ID_PARTENAIRE LEFT JOIN restaurant_menu_unite ON restaurant_menu_unite.ID_UNITE=restaurant_menu_taille.ID_UNITE LEFT JOIN  restaurant_menu_prix ON  restaurant_menu_prix.ID_RESTAURANT_MENU=restaurant_menu.ID_RESTAURANT_MENU WHERE ID_MENU_QUANTITE=?", [id]);
+        return query("SELECT * FROM restaurant_menu_taille rs_menu LEFT JOIN restaurant_menu ON restaurant_menu.ID_MENU_QUANTITE=rs_menu.ID_MENU_QUANTITE LEFT JOIN restaurant_categorie_menu ON restaurant_categorie_menu.ID_CATEGORIE_MENU=restaurant_menu.ID_CATEGORIE_MENU LEFT JOIN restaurant_sous_categorie_menu ON restaurant_sous_categorie_menu.ID_SOUS_CATEGORIE_MENU=restaurant_menu.ID_SOUS_CATEGORIE_MENU LEFT JOIN restaurant_sous_sous_categorie ON restaurant_sous_sous_categorie.ID_SOUS_SOUS_CATEGORIE=restaurant_menu.ID_SOUS_SOUS_CATEGORIE LEFT JOIN restaurant_repas ON restaurant_repas.ID_REPAS=restaurant_menu.ID_REPAS LEFT JOIN partenaires ON partenaires.ID_PARTENAIRE=restaurant_menu.ID_PARTENAIRE LEFT JOIN restaurant_menu_unite ON restaurant_menu_unite.ID_UNITE=rs_menu.ID_UNITE LEFT JOIN  restaurant_menu_prix ON  restaurant_menu_prix.ID_RESTAURANT_MENU=restaurant_menu.ID_RESTAURANT_MENU WHERE rs_menu.ID_MENU_QUANTITE", [id]);
     } catch (error) {
         throw error;
     }
