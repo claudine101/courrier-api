ea9192132309834b67cf01247264e598bf7b61e2
claudine101 <ndayisabaclaudine01@gmail.com>
2022-10-06 17:42:22 +0200

    verfication si le partenaire est inscrit dans un service

--- models/service.model.js
+++ models/service.model.js
@@ -14,8 +14,25 @@ const findAll = async ( value) => {
     }
 
 }
+const findById = async (ID_USER, ID_SERVICE) => {
+    try {
+      
+        var sqlQuery="SELECT * FROM partenaires par LEFT JOIN " 
+        sqlQuery+=" partenaire_service ser ON ser.ID_PARTENAIRE=par.ID_PARTENAIRE  "
+        sqlQuery+=" LEFT JOIN  services srv on srv.ID_SERVICE=ser.ID_SERVICE "
+        sqlQuery+=" WHERE par.ID_USER=? AND srv.ID_SERVICE=? "
+        return query(sqlQuery, [ID_USER, ID_SERVICE]);
+    }
+    catch (error) 
+    {
+        console.log(error)
+        throw error
+    }
+
+}
 module.exports = {
     findAll,
+    findById
 }
 
 
--- routes/service.routes.js
+++ routes/service.routes.js
@@ -2,4 +2,6 @@ const express = require('express')
 const servicecontroller = require('../controllers/service.controller')
 const serviceRouter = express.Router()
 serviceRouter.get('/', servicecontroller.findAllService)
+serviceRouter.get('/search/:ID_SERVICE', servicecontroller.findOne)
+
 module.exports = serviceRouter
