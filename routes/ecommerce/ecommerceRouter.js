1b6ccf2f36cd383085595a1bb2661ed52d469643
dukizwe <mbagapro@gmail.com>
2023-02-07 10:01:33 +0200

    refactory(resto): changer le nom du fichier du route des wishlist sur le module de resto

--- routes/ecommerce/ecommerceRouter.js
+++ routes/ecommerce/ecommerceRouter.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const ecommerce_commandes_routes = require('./ecommerce_commandes.routes')
 const ecommerce_produits_routes = require('./ecommerce_produits.routes')
-const ecommerce_wishlist_produit_routes=require('./ecommerce_wishlist_produit_routes')
+const ecommerce_wishlist_produit_routes=require('./ecommerce_wishlist_produit.routes')
 const ecommerceRouter = express.Router()
 
 ecommerceRouter.use('/ecommerce_wishlist_produit',ecommerce_wishlist_produit_routes)
